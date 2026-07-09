7545114d92f08264984a5912bcd0027434a10742
henil suchak <henilsuchak8311@gmail.com>
2026-07-09 11:43:49 +0530

    feat: implemented obstacle blocking in BackwardCommand

--- src/main/java/com/incubyte/marsrover/BackwardCommand.java
+++ src/main/java/com/incubyte/marsrover/BackwardCommand.java
@@ -5,8 +5,14 @@ public class BackwardCommand implements RoverCommand {
     @Override
     public void execute(Rover rover) {
         Direction direction = rover.getCurrentDirection();
-        int newXCoordinate = rover.getCurrentXCoordinate() - direction.getDeltaX();
-        int newYCoordinate = rover.getCurrentYCoordinate() - direction.getDeltaY();
-        rover.setPosition(newXCoordinate, newYCoordinate);
+        int targetXCoordinate = rover.getCurrentXCoordinate() - direction.getDeltaX();
+        int targetYCoordinate = rover.getCurrentYCoordinate() - direction.getDeltaY();
+
+        Grid grid = rover.getGrid();
+        if (grid.hasObstacleAt(targetXCoordinate, targetYCoordinate)) {
+            return;
+        }
+
+        rover.setPosition(targetXCoordinate, targetYCoordinate);
     }
 }
